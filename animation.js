2bf1646c554348f84ed2ba13d9405cf345dcd3b8
Lars <127956678+ainahh@users.noreply.github.com>
2024-01-12 08:46:30 +0100

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,26 @@
+
+//animations home page
+gsap.from(".logo", { opacity: 0, y: -50, duration: 0.5 });
+gsap.from(".counter", { opacity: 0, y: -50, duration: 0.4, delay: 0.4 });
+gsap.from(".buttonmain", { opacity: 0, y: 50, duration: 0.4, delay: 0.7 });
+gsap.from(".footer1", { opacity: 0, y: 50, duration: 0.3, delay: 0.8});
+
+
+//animations index page
+gsap.from(".muller", { opacity: 0, duration: 1.2, x: -50, scale: 1, delay: 0.9 });
+gsap.from(".title", { opacity: 0, duration: 1, y: 500 });
+
+//animations settings page
+gsap.from(".login", { opacity: 0, y: -50, duration: 0.4, delay: 0.6 });
+
+//login
+function checkPassword() {
+    var passwordInput = document.getElementById("password").value;
+    if (passwordInput === "asianbitches") {
+        // Correct password, redirect to a different page
+        window.location.href = "admin.html";
+    } else {
+        // Incorrect password, show an error message (optional)
+        alert("Wrong password. Please try again.");
+    }
+}
